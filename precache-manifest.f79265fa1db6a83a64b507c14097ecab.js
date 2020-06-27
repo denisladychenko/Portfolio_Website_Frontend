3f6116044b1845c04f0a27c3dd405fd559bfedc3
Denis Ladychenko <dsladychenko@gmail.com>
2020-06-27 03:46:14 -0400

    Updates

---
+++ precache-manifest.f79265fa1db6a83a64b507c14097ecab.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab82da5f076125ca2e9855d7cd0ed661",
+    "url": "/Portfolio_Website_Frontend/index.html"
+  },
+  {
+    "revision": "d89e6032335e5c262b97",
+    "url": "/Portfolio_Website_Frontend/static/css/main.36cadddc.chunk.css"
+  },
+  {
+    "revision": "24c46c198f684d6bc66f",
+    "url": "/Portfolio_Website_Frontend/static/js/2.b8d64e98.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portfolio_Website_Frontend/static/js/2.b8d64e98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d89e6032335e5c262b97",
+    "url": "/Portfolio_Website_Frontend/static/js/main.59990beb.chunk.js"
+  },
+  {
+    "revision": "0b43568fc3f640dd5dd8",
+    "url": "/Portfolio_Website_Frontend/static/js/runtime-main.7e43531c.js"
+  },
+  {
+    "revision": "c0e65ae65e511e85cb3f30f6f9a40fb8",
+    "url": "/Portfolio_Website_Frontend/static/media/My_photo_copy.c0e65ae6.PNG"
+  },
+  {
+    "revision": "88c0a75bb8a3fb1a99248759db2e300a",
+    "url": "/Portfolio_Website_Frontend/static/media/WSCC_copy_1.88c0a75b.png"
+  },
+  {
+    "revision": "7cb803f1b51d35c034c4c40c82be6c71",
+    "url": "/Portfolio_Website_Frontend/static/media/arizona-state-university.7cb803f1.png"
+  },
+  {
+    "revision": "07176b4ba6ced1eecc46d7e3be3f1341",
+    "url": "/Portfolio_Website_Frontend/static/media/myPicture.07176b4b.jpg"
+  },
+  {
+    "revision": "0d8c380293e2f99abdcf1cfcbdc62e42",
+    "url": "/Portfolio_Website_Frontend/static/media/my_family_2.0d8c3802.png"
+  },
+  {
+    "revision": "6fb3ec77d801b2b439cfe156f25a7b93",
+    "url": "/Portfolio_Website_Frontend/static/media/notebook.6fb3ec77.png"
+  },
+  {
+    "revision": "ad70581f12eec5030d6e93e16bfa2993",
+    "url": "/Portfolio_Website_Frontend/static/media/notebook_flipped.ad70581f.png"
+  },
+  {
+    "revision": "d224cc8b23a4c9eda4faf1b7a0f9b95f",
+    "url": "/Portfolio_Website_Frontend/static/media/snowboard_1.d224cc8b.png"
+  }
+]);
