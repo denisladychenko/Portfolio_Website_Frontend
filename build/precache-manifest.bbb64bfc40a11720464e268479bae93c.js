f5c301a749e08e728bf96544e10837364fb44f99
Denis Ladychenko <dsladychenko@gmail.com>
2021-01-10 17:10:10 -0500

    removed .gitignore

---
+++ build/precache-manifest.bbb64bfc40a11720464e268479bae93c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff61e6ab85e344ec9c2a0af137c3199c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65e18488cc041266bdf0",
+    "url": "/static/css/3.407e1ec2.chunk.css"
+  },
+  {
+    "revision": "01f1c5a07bf3d3cb717c",
+    "url": "/static/css/4.e3b4cc17.chunk.css"
+  },
+  {
+    "revision": "d74cb241aa34e3335f7a",
+    "url": "/static/css/5.be00a98e.chunk.css"
+  },
+  {
+    "revision": "7d0cc7c255c659b5385c",
+    "url": "/static/css/main.244a6d0b.chunk.css"
+  },
+  {
+    "revision": "3963d092edb58a14b860",
+    "url": "/static/js/2.a9292078.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/static/js/2.a9292078.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65e18488cc041266bdf0",
+    "url": "/static/js/3.5408276f.chunk.js"
+  },
+  {
+    "revision": "01f1c5a07bf3d3cb717c",
+    "url": "/static/js/4.834ef85f.chunk.js"
+  },
+  {
+    "revision": "d74cb241aa34e3335f7a",
+    "url": "/static/js/5.e7577db1.chunk.js"
+  },
+  {
+    "revision": "9d485ccedd36a31dd8b9",
+    "url": "/static/js/6.edbf6ac7.chunk.js"
+  },
+  {
+    "revision": "7d0cc7c255c659b5385c",
+    "url": "/static/js/main.48d264eb.chunk.js"
+  },
+  {
+    "revision": "d339f3c262f178cd7b0b",
+    "url": "/static/js/runtime-main.39dd3d95.js"
+  }
+]);
