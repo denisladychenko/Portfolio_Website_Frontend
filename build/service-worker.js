02e55b4ccac9129e1891808ca27e07766a93bfa7
Denis Ladychenko <dsladychenko@gmail.com>
2022-11-19 16:33:12 -0500

    New Build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3b167ac9c36c4c7c905107bb5bad9ae8.js"
+  "/precache-manifest.989b9985edca723f6da09d0e8951b9d0.js"
 );
 
 self.addEventListener('message', (event) => {
